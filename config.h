c67d1a54c99cb54e424fddd8d50e6abf7fe83b7e
johndovern <92282293+johndovern@users.noreply.github.com>
2022-02-13 00:42:17 -0800

    update

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"FantasqueSansMono Nerd Fonts Mono:size=10",
+	"monospace:size=10",
 	"JoyPixels:pixelsize=12:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
