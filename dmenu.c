f53013eaf5c385942c0a046cf15871a09dedf621
johndovern <92282293+johndovern@users.noreply.github.com>
2025-06-02 18:00:20 -0700

    up: fix_incorrect_comment__math_is_hard

--- dmenu.c
+++ dmenu.c
@@ -1135,7 +1135,7 @@ setup(void)
 		}
 	}
 	promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
-	inputw = mw / 3; /* input width: ~30% of monitor width */
+	inputw = mw / 3; /* input width: ~33% of monitor width */
 	match();
 
 	/* create menu window */
