20fa0591f4188d398420e88504d998f75e414fab
johndovern <92282293+johndovern@users.noreply.github.com>
2025-06-02 18:14:19 -0700

    up: remove_extra_newline

--- dmenu.c
+++ dmenu.c
@@ -1156,7 +1156,6 @@ setup(void)
 		XSetWindowBorder(dpy, win, scheme[SchemeSel][ColBorder].pixel);
 	XSetClassHint(dpy, win, &ch);
 
-
 	/* input methods */
 	if ((xim = XOpenIM(dpy, NULL, NULL, NULL)) == NULL)
 		die("XOpenIM failed: could not open input device");
